f477f6137461c34f2e39d8f612dfa9b555b568f4
HarshalRawal <harshalrawal463@gmail.com>
2025-01-24 20:41:05 +0530

    added frontend and completed workFlow creation

--- backend/src/routes/v1-Routes/main.ts
+++ backend/src/routes/v1-Routes/main.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { getAllApps } from "../../controllers/get-all-apps";
 import { getAppTriggers , checkIsConnectionRequired} from "../../controllers/triggers";
+import { googleAuth,googleAuthCallback} from "../../controllers/google-auth";
+import { createWorkflow } from "../../controllers/create-workFlow";
 const router = Router();
 
 router.route("/").get((req,res)=>{
@@ -9,4 +11,7 @@ router.route("/").get((req,res)=>{
 router.route("/apps").get(getAllApps);
 router.route("/triggers/:appId").get(getAppTriggers);
 router.route("/triggers/connection-required/:key").get(checkIsConnectionRequired);
+router.route("/auth/google").get(googleAuth);
+router.route("/auth/google/callback").get(googleAuthCallback);
+router.route("/workflow").post(createWorkflow);
 export default router;
